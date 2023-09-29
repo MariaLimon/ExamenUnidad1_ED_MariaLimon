4c5361c14674297b646d6e8efdcb49de7c389454
MariaLimon <limon185mari@gmail.com>
2023-09-29 09:16:19 -0700

    Agregar archivos de proyecto.

---
+++ Clases/Ejercicios.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenUnidad1_ED_MariaLimon.Clases
+{
+    internal class Ejercicios
+    {
+        //public int TablaMultiplicar(int numero, int contador)
+        //{
+            
+        //}
+
+        public void IntervaloNumeros (int numero1, int numero2)
+        {
+            if (numero1 != numero2)
+            {
+                if(numero1 <= numero2)
+                {
+                    while (numero1 < numero2)
+                    {
+                        Console.WriteLine($"la secuencia de numeros es {numero1++}");
+                    
+                    }
+                }
+                while (numero1 >= numero2)
+                {
+                    Console.WriteLine($"la secuencia de numero es {numero1--}");
+                }
+                
+            }
+            Console.WriteLine ("los numeros son iguales");
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using ExamenUnidad1_ED_MariaLimon.Clases;
+
+namespace ExamenUnidad1_ED_MariaLimon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Console.WriteLine("Dame un numero");
+            //int numero = Convert.ToInt32(Console.ReadLine());
+            //Ejercicios ejercicio1 = new Ejercicios();
+            //Console.WriteLine(ejercicio1.TablaMultiplicar(numero,0));
+            
+            
+            Console.WriteLine("dame un numero");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("dame otro numero");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            Ejercicios ejercicio2 = new Ejercicios();
+            ejercicio2.IntervaloNumeros(numero1, numero2);
+            
+        
+        }
+    }
+}
